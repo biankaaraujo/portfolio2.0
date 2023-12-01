25661c99c6bbcc262e03f86dd20892306a13eade
Bianka Araujo <bianka_chris@hotmail.com>
2023-12-01 15:53:08 -0300

    Criado popUp e evento para abrir e fechar o mesmo

--- script.js
+++ script.js
@@ -45,3 +45,65 @@ menuButton.addEventListener("click", function () {
     menu.style.display = "flex";
   }
 });
+
+// Evento de clique para abrir/fechar mais detalhes sobre o projeto
+document.querySelectorAll("#projetos .itensProjetos .item").forEach((item) => {
+  item.addEventListener("click", function (e) {
+    const projetoIdx = this.getAttribute("idx");
+    const popUp = document.getElementById(projetoIdx).parentElement;
+
+    popUp.style.transition = "opacity 0.5s ease-in-out";
+    popUp.style.opacity = "1";
+    popUp.style.display = "inline-block";
+  });
+});
+
+function closePopupWithButton(event) {
+  const parentElement = event.parentElement.parentElement;
+
+  parentElement.style.opacity = "0";
+  setTimeout(() => {
+    parentElement.style.display = "none";
+    parentElement.style.transition = "";
+  }, 500);
+}
+
+function closePopup(popUp) {
+  popUp.style.opacity = "0";
+  setTimeout(() => {
+    popUp.style.display = "none";
+    popUp.style.transition = "";
+  }, 500);
+}
+
+// novo
+document.querySelectorAll("#projetos .itensProjetos .item").forEach((item) => {
+  item.addEventListener("click", function (e) {
+    const projetoIdx = this.getAttribute("idx");
+    const popUp = document.getElementById(projetoIdx).parentElement;
+
+    openPopup(popUp);
+  });
+});
+
+function openPopup(popUp) {
+  popUp.style.transition = "opacity 0.5s ease-in-out";
+  popUp.style.opacity = "1";
+  popUp.style.display = "inline-block";
+
+  // Adiciona evento para fechar ao pressionar "Esc"
+  document.addEventListener("keydown", function closeOnEsc(event) {
+    if (event.key === "Escape") {
+      closePopup(popUp);
+      document.removeEventListener("keydown", closeOnEsc);
+    }
+  });
+
+  // Adiciona evento para fechar ao clicar fora do popup
+  popUp.addEventListener("click", function closeOnClickOutside(event) {
+    if (event.target === popUp) {
+      closePopup(popUp);
+      popUp.removeEventListener("click", closeOnClickOutside);
+    }
+  });
+}
